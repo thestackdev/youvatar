f11fb737082281e0b602cd081c88f07dcf628628
thestackdev <thestackdev@fullstacklab.org>
2023-02-26 17:52:43 +0530

    first commit

--- pages/cirriculum.jsx
+++ pages/cirriculum.jsx
@@ -1,21 +1,88 @@
 import SemiCircleProgressBar from "@/components/Progress";
 import axios from "axios";
-import Link from "next/link";
 import { useState } from "react";
 
 export default function Dashbaord() {
   const [progress, setProgress] = useState(30);
+  const [modues, setModules] = useState([
+    {
+      id: 1,
+      label: "introduction",
+      lectures: [
+        {
+          id: 1,
+          label: "introduction",
+        },
+        {
+          id: 2,
+          label: "getting started",
+        },
+      ],
+    },
+    {
+      id: 2,
+      label: "getting started",
+      lectures: [
+        {
+          id: 1,
+          label: "introduction",
+        },
+        {
+          id: 2,
+          label: "hands on",
+        },
+      ],
+    },
+  ]);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  const handleCreateModule = async () => {
     try {
       const form = new URLSearchParams();
 
+      form.append("module_number", 1);
+      form.append("module_name", "dummy module");
+      form.append("course_id", 4);
       const response = await axios.post(
-        "/courses/create_course/landing_page",
+        "/courses/create_course/module",
         form.toString()
       );
       console.log(response.data);
+      setModules((e) => [
+        ...e,
+        { id: Date.now(), label: "dummy module", lectures: [] },
+      ]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleCreateLecture = async (id) => {
+    try {
+      const filtered = modues.filter((e) => e.id === id);
+
+      const form = new URLSearchParams();
+
+      form.append("lecture_number", filtered[0].lectures.length + 1);
+      form.append("lecture_name", "dummy lecture");
+      form.append("module_id", id);
+      const response = await axios.post(
+        "/courses/create_course/lecture",
+        form.toString()
+      );
+      console.log(response.data);
+
+      const updated = modues.map((e) => {
+        if (e.id === id) {
+          e.lectures.push({
+            id: Date.now(),
+            label: "dummy lecture",
+          });
+          return e;
+        }
+        return e;
+      });
+
+      setModules(updated);
     } catch (error) {
       console.log(error);
     }
@@ -36,7 +103,7 @@ export default function Dashbaord() {
           className="h-[548px] w-[265px] rounded-sm hidden lg:block"
           src="/assets/mobile.png"
         />
-        <form onSubmit={handleSubmit} className="flex flex-col">
+        <div className="flex flex-col">
           <h1 className="text-[#121212] font-bold text-3xl mb-2">Curriculum</h1>
           <div className="flex flex-row gap-4">
             <img className="w-full lg:w-[300px]" src="/assets/girl.png" />
@@ -58,38 +125,48 @@ export default function Dashbaord() {
             Start putting together your course by creating sections, lectures
             and practice
           </p>
-          <div className="flex flex-col border border-[#C7C9D9] rounded mt-4 p-4">
-            <h1>
-              <b>Module 1:</b> Introduction
-            </h1>
-            <div className="block md:flex flex-col sm:flex-row border border-[#C7C9D9] justify-between items-center m-2 p-2 rounded">
-              <h1>
-                <b>Lecture 1:</b> Introduction
-              </h1>
-              <div className="flex gap-2 m-2">
-                <button
-                  type="submit"
-                  className="bg-white border md:border-none border-[#3949AB] text-[#3949AB] rounded px-3 py-2 text-sm"
-                >
-                  Add Assignments
-                </button>
-                <button
-                  type="submit"
-                  className="bg-[#3949AB] text-white rounded px-3 py-2 text-sm"
-                >
-                  Add Resouce
-                </button>
-              </div>
-            </div>
-            <Link
-              href="/dashboard"
-              className="bg-[#3949AB] text-white rounded px-3 py-2 text-sm w-fit"
+          {modues.map((module, moduleIndex) => (
+            <div
+              key={moduleIndex}
+              className="flex flex-col border border-[#C7C9D9] rounded mt-4 p-4"
             >
-              Add next lecture
-            </Link>
-          </div>
+              <h1 className="capitalize">
+                <b>Module {moduleIndex}:</b> {module.label}
+              </h1>
+              {module.lectures.map((lecture, lectureIndex) => (
+                <div
+                  key={lectureIndex}
+                  className="block md:flex flex-col sm:flex-row border border-[#C7C9D9] justify-between items-center m-2 p-2 rounded"
+                >
+                  <h1 className="capitalize">
+                    <b>Lecture {lectureIndex}:</b> {lecture.label}
+                  </h1>
+                  <div className="flex gap-2 m-2">
+                    <button
+                      type="submit"
+                      className="bg-white border md:border-none border-[#3949AB] text-[#3949AB] rounded px-3 py-2 text-sm"
+                    >
+                      Add Assignments
+                    </button>
+                    <button
+                      type="submit"
+                      className="bg-[#3949AB] text-white rounded px-3 py-2 text-sm"
+                    >
+                      Add Resouce
+                    </button>
+                  </div>
+                </div>
+              ))}
+              <button
+                onClick={() => handleCreateLecture(module.id)}
+                className="bg-[#3949AB] text-white rounded px-3 py-2 text-sm w-fit mt-2"
+              >
+                Add next lecture
+              </button>
+            </div>
+          ))}
           <button
-            type="submit"
+            onClick={handleCreateModule}
             className="bg-[#3949AB] text-white rounded px-3 py-2 text-sm w-fit mt-4"
           >
             Add New Module
@@ -100,7 +177,7 @@ export default function Dashbaord() {
           >
             Proceed
           </button>
-        </form>
+        </div>
       </div>
     </div>
   );
